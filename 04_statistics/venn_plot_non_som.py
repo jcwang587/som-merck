229b7ed734f9557a6cebf57645d8417eb5d9e454
jcwang587 <jiachengwang@umass.edu>
2025-01-03 12:00:29 -0500

    add venn plot

--- 04_statistics/venn_plot_non_som.py
+++ 04_statistics/venn_plot_non_som.py
@@ -8,22 +8,22 @@ import pandas as pd
 
 # Load the data
 all_data = pd.read_csv("../data/dataset/dataset_merck_all.csv")
-bde_som = pd.read_csv("../data/bins/bde_som/low_bde_som.csv")
-relative_ir_som = pd.read_csv("../data/bins/relative_ir_som/high_relative_ir_som.csv")
-sasa_som = pd.read_csv("../data/bins/sasa_som/high_sasa_maestro_som.csv")
+bde_non_som = pd.read_csv("../data/bins/bde_som/low_bde_non_som.csv")
+relative_ir_non_som = pd.read_csv("../data/bins/relative_ir_som/high_relative_ir_non_som.csv")
+sasa_non_som = pd.read_csv("../data/bins/sasa_som/high_sasa_maestro_non_som.csv")
 
-print(len(bde_som))
-print(len(relative_ir_som))
-print(len(sasa_som))
+print(len(bde_non_som))
+print(len(relative_ir_non_som))
+print(len(sasa_non_som))
 
 # Plot the venn diagram
 plt.figure(figsize=(10, 8))
 
 v = venn3(
     [
-        set(bde_som["zaretzki_atomic_index"]),
-        set(relative_ir_som["zaretzki_atomic_index"]),
-        set(sasa_som["zaretzki_atomic_index"]),
+        set(bde_non_som["zaretzki_atomic_index"]),
+        set(relative_ir_non_som["zaretzki_atomic_index"]),
+        set(sasa_non_som["zaretzki_atomic_index"]),
     ],
     ["Low BDE", "High Relative IR", "High SASA"],
     layout_algorithm=matplotlib_venn.layout.venn3.DefaultLayoutAlgorithm(fixed_subset_sizes=(1,1,1,1,1,1,1))
@@ -38,9 +38,9 @@ for label, color in zip(v.set_labels, label_colors):
 
 c = venn3_circles(
     [
-        set(bde_som["zaretzki_atomic_index"]),
-        set(relative_ir_som["zaretzki_atomic_index"]),
-        set(sasa_som["zaretzki_atomic_index"]),
+        set(bde_non_som["zaretzki_atomic_index"]),
+        set(relative_ir_non_som["zaretzki_atomic_index"]),
+        set(sasa_non_som["zaretzki_atomic_index"]),
     ],
     linestyle="dashed",
     linewidth=2,
@@ -54,35 +54,35 @@ for text in v.subset_labels:
         text.set_fontweight("bold")
 
 # Find the unique zaretzki_atomic_index
-unique_index = set(bde_som["zaretzki_atomic_index"]) | set(relative_ir_som["zaretzki_atomic_index"]) | set(sasa_som["zaretzki_atomic_index"])
+unique_index = set(bde_non_som["zaretzki_atomic_index"]) | set(relative_ir_non_som["zaretzki_atomic_index"]) | set(sasa_non_som["zaretzki_atomic_index"])
 
 
 # Add text at the right bottom of the plot
 plt.text(0.7, 0.2, f"Others: 1", fontsize=24, fontweight="bold", transform=plt.gcf().transFigure)
 
 
-plt.savefig("./venn_plot_som.png", dpi=300, bbox_inches="tight")
+plt.savefig("./venn_plot_non_som.png", dpi=300, bbox_inches="tight")
 
 # print the zaretzki_atomic_index of the atoms in low bde only
-low_bde_only = set(bde_som["zaretzki_atomic_index"]) - set(relative_ir_som["zaretzki_atomic_index"]) - set(sasa_som["zaretzki_atomic_index"])
+low_bde_only = set(bde_non_som["zaretzki_atomic_index"]) - set(relative_ir_non_som["zaretzki_atomic_index"]) - set(sasa_non_som["zaretzki_atomic_index"])
 print(low_bde_only)
 print(len(low_bde_only))    
 
 # print the zaretzki_atomic_index of the atoms in high relative ir only
-high_relative_ir_only = set(relative_ir_som["zaretzki_atomic_index"]) - set(bde_som["zaretzki_atomic_index"]) - set(sasa_som["zaretzki_atomic_index"])
+high_relative_ir_only = set(relative_ir_non_som["zaretzki_atomic_index"]) - set(bde_non_som["zaretzki_atomic_index"]) - set(sasa_non_som["zaretzki_atomic_index"])
 print(high_relative_ir_only)
 print(len(high_relative_ir_only))    
 
 # print the zaretzki_atomic_index of the atoms in high sasa only
-high_sasa_only = set(sasa_som["zaretzki_atomic_index"]) - set(bde_som["zaretzki_atomic_index"]) - set(relative_ir_som["zaretzki_atomic_index"])
+high_sasa_only = set(sasa_non_som["zaretzki_atomic_index"]) - set(bde_non_som["zaretzki_atomic_index"]) - set(relative_ir_non_som["zaretzki_atomic_index"])
 print(high_sasa_only)
 print(len(high_sasa_only))    
 
 # print the zaretzki_atomic_index of the atoms 
 # remove na rows
 all_data = all_data.dropna()
-all_som_index = all_data[all_data["som"] == 1]["zaretzki_atomic_index"].tolist()
+all_non_som_index = all_data[all_data["som"] == 0]["zaretzki_atomic_index"].tolist()
 
-others = set(all_som_index) - unique_index
+others = set(all_non_som_index) - unique_index
 print(others)
 print(len(others))    
