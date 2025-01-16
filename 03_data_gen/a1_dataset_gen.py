fbf4db93b19bc3b48a15c596df165d68d47a38a5
jcwang587 <jiachengwang@umass.edu>
2025-01-16 17:16:01 -0500

    split to bde only

--- 03_data_gen/a1_dataset_gen.py
+++ 03_data_gen/a1_dataset_gen.py
@@ -323,7 +323,7 @@ dataset = dataset[dataset["hydrogen_neighbor"] > 0]
 dataset = dataset[dataset["double_bonded"] == 0]
 
 # Export the dataset to csv
-dataset.to_csv("../data/dataset/dataset_merck_all.csv", index=False)
+dataset.to_csv("../data/dataset/dataset_merck_bde.csv", index=False)
 
 # Print the ratio of SOM and non-SOM
 primary_som_count = dataset["primary_som"].sum()
