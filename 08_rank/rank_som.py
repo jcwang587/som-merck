ed3dd9d2a6e86a073888d43bdd4302d64c144903
jcwang587 <jiachengwang@umass.edu>
2025-01-24 15:34:10 -0500

    checkpoint

--- 08_rank/rank_som.py
+++ 08_rank/rank_som.py
@@ -7,22 +7,26 @@ data_bde = data[['zaretzki_index', 'bde', 'som']].copy()
 data_bde['bde_rank'] = data_bde.groupby('zaretzki_index')['bde'].rank(ascending=True)
 data_bde.to_csv('./dataset_merck_bde_rank.csv', index=False)
 
-# Get the total number of som
-som_count = data_bde[data_bde['som'] == 1].shape[0]
+# Get the count of unique zaretzki_index
+unique_zaretzki_index = data_bde['zaretzki_index'].nunique()
+print(f"number of unique zaretzki_index: {unique_zaretzki_index}")
 
 # Initialize counters
-top_counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+top_counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
 
 # Group by 'zaretzki_index' and iterate over each group
+group_count = 0
 for _, group in data_bde.groupby('zaretzki_index'):
-    for rank in range(1, 6):
+    for rank in range(1, 11):
         if any((group['som'] == 1) & (group['bde_rank'] == rank)):
             top_counts[rank] += 1
             break 
+    group_count += 1
 
+print(f"group count: {group_count}")
 # Print the results
 print("bde")
-for rank in range(1, 6):
+for rank in range(1, 11):
     print(f"Top {rank} count: {top_counts[rank]}")
 
 
@@ -34,15 +38,15 @@ data_ir.to_csv('./dataset_merck_ir_rank.csv', index=False)
 som_count = data_ir[data_ir['som'] == 1].shape[0]
 
 # Initialize counters
-top_counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0} 
+top_counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0} 
 
 for _, group in data_ir.groupby('zaretzki_index'):
-    for rank in range(1, 6):
+    for rank in range(1, 11):
         if any((group['som'] == 1) & (group['ir_rank'] == rank)):
             top_counts[rank] += 1
             break 
 print("intrinsic reactivity")
-for rank in range(1, 6):
+for rank in range(1, 11):
     print(f"Top {rank} count: {top_counts[rank]}")
 
 
