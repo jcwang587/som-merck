5188fa67ba10c3f9a66b486db70c9ff0c895d2dc
jcwang587 <jiachengwang@umass.edu>
2025-01-24 15:19:39 -0500

    checkpoint

--- 08_rank/rank_som.py
+++ 08_rank/rank_som.py
@@ -15,14 +15,34 @@ top_counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
 
 # Group by 'zaretzki_index' and iterate over each group
 for _, group in data_bde.groupby('zaretzki_index'):
-    # Check for the first occurrence of 'som' in the top 5 ranks
     for rank in range(1, 6):
         if any((group['som'] == 1) & (group['bde_rank'] == rank)):
             top_counts[rank] += 1
-            break  # Stop counting further for this molecule
+            break 
 
 # Print the results
-print(f"som count: {som_count}")
+print("bde")
 for rank in range(1, 6):
     print(f"Top {rank} count: {top_counts[rank]}")
+
+
+data_ir = data[['zaretzki_index', 'ir', 'som']].copy()
+data_ir['ir_rank'] = data_ir.groupby('zaretzki_index')['ir'].rank(ascending=False)
+data_ir.to_csv('./dataset_merck_ir_rank.csv', index=False)
+
+# Get the total number of som
+som_count = data_ir[data_ir['som'] == 1].shape[0]
+
+# Initialize counters
+top_counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0} 
+
+for _, group in data_ir.groupby('zaretzki_index'):
+    for rank in range(1, 6):
+        if any((group['som'] == 1) & (group['ir_rank'] == rank)):
+            top_counts[rank] += 1
+            break 
+print("intrinsic reactivity")
+for rank in range(1, 6):
+    print(f"Top {rank} count: {top_counts[rank]}")
+
 
