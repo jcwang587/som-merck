037724390818dc2e27a7ed5259ba281a5e5d8adc
jcwang587 <jiachengwang@umass.edu>
2025-01-29 16:10:38 -0500

    checkpoint

--- 05_bde_report/v2/01_bde_sasa.py
+++ 05_bde_report/v2/01_bde_sasa.py
@@ -4,12 +4,25 @@ from schrodinger.structure import StructureReader
 
 
 def combine_bde_sasa(bde_file, sasa_file):
-    structure_bde = StructureReader(bde_file)
-    structure_sasa = StructureReader(sasa_file)
+    structure_bde = StructureReader.read(bde_file)
+    structure_sasa = StructureReader.read(sasa_file)
+
+    bde_dict = {}
+    # Get the SASA property r_user_sasa
+    for atom in structure_sasa.atom:
+        if "r_user_sasa" in atom.property:
+            bde_dict[atom.index] = atom.property["r_user_sasa"]
+
+    # Add the SASA property r_user_sasa to the BDE structure
+    for atom in structure_bde.atom:
+        if atom.index in bde_dict:
+            atom.property["r_user_sasa"] = bde_dict[atom.index]
+
+    # Export the combined structure
+    structure_bde.write("combined_bde_sasa.mae")
 
 
-
-bde_file = "05_bde_report/v2/bde_data.sdf"
-sasa_file = "05_bde_report/v2/sasa_data.sdf"
+bde_file = "./bde_sasa/015_CARBAMAZEPINE.mae"
+sasa_file = "./bde_sasa/structure_015_sasa.mae"
 
 combine_bde_sasa(bde_file, sasa_file)
