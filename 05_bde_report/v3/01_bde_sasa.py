a2eda3276b871f19f603a67ac72774621c380a15
jcwang587 <jiachengwang@umass.edu>
2025-02-10 10:13:30 -0500

    add csv results

--- 05_bde_report/v3/01_bde_sasa.py
+++ 05_bde_report/v3/01_bde_sasa.py
@@ -3,10 +3,11 @@
 import os
 import shutil
 from schrodinger.structure import StructureReader
+import pandas as pd
 
 
-def combine_bde_mae_sasa(bde_file, sasa_file, output_file):
-    structure_bde = StructureReader.read(bde_file)
+def combine_bde_mae_sasa(bde_mae_file, sasa_file, output_file):
+    structure_bde = StructureReader.read(bde_mae_file)
     structure_sasa = StructureReader.read(sasa_file)
 
     bde_dict = {}
@@ -24,20 +25,41 @@ def combine_bde_mae_sasa(bde_file, sasa_file, output_file):
     structure_bde.write(output_file)
 
 
+def combine_bde_csv_sasa(bde_csv_file, sasa_csv_file, output_file):
+    df_bde = pd.read_csv(bde_csv_file)
+    structure_sasa = StructureReader.read(sasa_csv_file)
 
-bde_dir = "./bde_mae"
-bde_files = os.listdir(bde_dir)
+    bde_dict = {}
+    # Add the BDE property to the BDE structure
+    for index, row in df_bde.iterrows():
+        # get the atom index from the column "Atom" remove the "C"
+        atom_index = int(row["Atom"].replace("C", ""))
+        bde_dict[atom_index] = row["BDE (kcal/mol)"]
 
-sasa_dir = "./sasa"
-sasa_files = os.listdir(sasa_dir)
+    # Add the BDE property to the SASA structure
+    for atom in structure_sasa.atom:
+        if atom.index in bde_dict:
+            atom.property["r_user_CH-BDE"] = bde_dict[atom.index]
+
+    # Export the combined structure
+    structure_sasa.write(output_file)
+
+
+bde_mae_dir = "./bde_mae"
+bde_mae_files = os.listdir(bde_mae_dir)
+bde_mae_files.sort()
+
+sasa_mae_dir = "./sasa_mae"
+sasa_mae_files = os.listdir(sasa_mae_dir)
+sasa_mae_files.sort()
 
 output_dir = "./output"
 shutil.rmtree(output_dir, ignore_errors=True)
 os.makedirs(output_dir, exist_ok=True)
 
-for bde_file, sasa_file in zip(bde_files, sasa_files):
-    bde_path = f"{bde_dir}/{bde_file}"
-    sasa_path = f"{sasa_dir}/{sasa_file}"
+for bde_file, sasa_file in zip(bde_mae_files, sasa_mae_files):
+    bde_path = f"{bde_mae_dir}/{bde_file}"
+    sasa_path = f"{sasa_mae_dir}/{sasa_file}"
 
     bde_file = os.path.basename(bde_path)
     sasa_file = os.path.basename(sasa_path)
@@ -46,3 +68,24 @@ for bde_file, sasa_file in zip(bde_files, sasa_files):
     output_file = f"{output_dir}/{structure.title}_bde_sasa.mae"
     combine_bde_mae_sasa(bde_path, sasa_path, output_file)
     print(f"Combined {bde_file} and {sasa_file} into {output_file}")
+
+
+bde_csv_dir = "./bde_csv"
+bde_csv_files = os.listdir(bde_csv_dir)
+bde_csv_files.sort()
+
+sasa_csv_dir = "./sasa_csv"
+sasa_csv_files = os.listdir(sasa_csv_dir)
+sasa_csv_files.sort()
+
+for bde_csv_file, sasa_csv_file in zip(bde_csv_files, sasa_csv_files):
+    bde_csv_path = f"{bde_csv_dir}/{bde_csv_file}"
+    sasa_csv_path = f"{sasa_csv_dir}/{sasa_csv_file}"
+
+    bde_file = os.path.basename(bde_csv_path)
+    sasa_file = os.path.basename(sasa_csv_path)
+
+    structure = StructureReader.read(sasa_csv_path)
+    output_file = f"{output_dir}/{structure.title}_bde_sasa.mae"
+    combine_bde_csv_sasa(bde_csv_path, sasa_csv_path, output_file)
+    print(f"Combined {bde_file} and {sasa_file} into {output_file}")    
