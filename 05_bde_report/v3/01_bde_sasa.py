b16e5e344be3e99dcf3bd3d637f7826ef1a616ed
jcwang587 <jiachengwang@umass.edu>
2025-02-10 09:36:08 -0500

    add bde csv

--- 05_bde_report/v3/01_bde_sasa.py
+++ 05_bde_report/v3/01_bde_sasa.py
@@ -5,7 +5,7 @@ import shutil
 from schrodinger.structure import StructureReader
 
 
-def combine_bde_sasa(bde_file, sasa_file, output_file):
+def combine_bde_mae_sasa(bde_file, sasa_file, output_file):
     structure_bde = StructureReader.read(bde_file)
     structure_sasa = StructureReader.read(sasa_file)
 
@@ -25,7 +25,7 @@ def combine_bde_sasa(bde_file, sasa_file, output_file):
 
 
 
-bde_dir = "./bde"
+bde_dir = "./bde_mae"
 bde_files = os.listdir(bde_dir)
 
 sasa_dir = "./sasa"
@@ -36,13 +36,13 @@ shutil.rmtree(output_dir, ignore_errors=True)
 os.makedirs(output_dir, exist_ok=True)
 
 for bde_file, sasa_file in zip(bde_files, sasa_files):
-    bde_path = f"./bde/{bde_file}"
-    sasa_path = f"./sasa/{sasa_file}"
+    bde_path = f"{bde_dir}/{bde_file}"
+    sasa_path = f"{sasa_dir}/{sasa_file}"
 
     bde_file = os.path.basename(bde_path)
     sasa_file = os.path.basename(sasa_path)
 
     structure = StructureReader.read(bde_path)
-    output_file = f"./output/{structure.title}_bde_sasa.mae"
-    combine_bde_sasa(bde_path, sasa_path, output_file)
+    output_file = f"{output_dir}/{structure.title}_bde_sasa.mae"
+    combine_bde_mae_sasa(bde_path, sasa_path, output_file)
     print(f"Combined {bde_file} and {sasa_file} into {output_file}")
